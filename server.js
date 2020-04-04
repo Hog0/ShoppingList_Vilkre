0e5d727c1c248dfe3647021a75a8cbede0ef527e
Vilkre <hinnar123.vilkr@outlook.com>
2020-04-04 15:10:36 +0300

    done

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb+srv://Vaikehugo:<Minubella1>@cluster-wjtkt.azure.mongodb.net/test?retryWrites=true&w=majority/ShoppingListDB', {
+ useNewUrlParser: true, useUnifiedTopology: true });
+
+//create item schema
+const itemSchema = {
+    name: String
+};
+
+//create mongoose model, models are always in capital
+const Item = mongoose.model('Item', itemSchema);
+
+
+//const items = [];
+//const itemsStudy = [];
+
+app.get('/', (req, res)=>{
+    let today = new Date();
+    //to get the date and the day of the week
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    
+    let day = today.toLocaleDateString("en-US", options);
+    //console.log(day);
+
+    //{} find all the items inside the collection
+    Item.find({}, function(error, fetchedItems){
+        console.log(fetchedItems);
+        res.render('index', {
+            listTitle: day,
+            newListItems: fetchedItems
+        });
+    });
+
+    
+});
+
+
+app.post('/', (req, res)=>{
+    console.log(req.body.list);
+    let userInput = req.body.newItem;
+
+    const item = new Item({
+        name: userInput
+    });
+
+    item.save();
+    res.redirect('/');
+   
+});
+
+app.post('/delete', (req, res) => {
+    console.log(req.body.checkbox);
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, function(error) {
+        if(!error){
+            console.log("Successfully deleted checked item!");
+            res.redirect('/');
+        }
+    });
+});
+
+
+let port = 3000;
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log('Server is running on port '+ port);    
+})
